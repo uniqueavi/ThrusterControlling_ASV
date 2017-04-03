0e51383b1add96522dcb691bfb15d45146b820ff
Avirup Ghosh <avirup.ece2013@gmail.com>
2017-04-03 18:41:06 +0530

    Add files via upload

---
+++ hardware_pwm_50hz.ino
@@ -0,0 +1,21 @@
+#include <TimerOne.h>
+ 
+void setup() 
+{
+
+  pinMode(13, OUTPUT);    
+  
+  Timer1.initialize(10000); 
+  Timer1.attachInterrupt( timerIsr ); 
+}
+ 
+void loop()
+{
+  // Main code loop
+}
+ 
+void timerIsr()
+{
+    // generating pwm
+    digitalWrite( 13, digitalRead( 13 ) ^ 1 );
+}
